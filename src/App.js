2272e378e0c48b947c4dabf95cb472bb47a55c5b
Umesh11032000 <umeshprajapati1317@gmail.com>
2020-08-31 19:13:40 +0530

    adding 404 reuqest

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
@@ -52,10 +52,11 @@ class App extends React.Component {
             </Navbar.Collapse>
           </Navbar>
 
-          <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
-          <Route path="/about" render={() => <AboutPage title={this.state.about.title} />} />
-          <Route path="/contact" render={() => <ContactPage title={this.state.contact.title} />} />
-          
+          <Route exact path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
+          <Route exact path="/about" render={() => <AboutPage title={this.state.about.title} />} />
+          <Route exact path="/contact" render={() => <ContactPage title={this.state.contact.title} />} />
+          <Redirect to="/" /> 
+
           <Footer />
 
         </Container>
